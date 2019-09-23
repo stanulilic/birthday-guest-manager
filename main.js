4221d4a981b18c465ec00ea2f44792b8d8008c4e
Stanley Ulili <stanleyulili@gmail.com>
2019-09-23 13:33:34 +0200

    (refactor): birthday field behaves as expected with the refactored code

--- main.js
+++ main.js
@@ -2,9 +2,45 @@
 const birthdayDateField = document.getElementById('birthday-date');
 const expectedGuestsField = document.getElementById('expected-guests');
 const setupBirthdayForm = document.querySelector('.setup-birthday');
+const formInputs = document.querySelectorAll('input');
 
+const fieldsData = {
+  birthdayField: {
+    regex:/20\d{2}-\d{2}-\d{2}/, 
+    message: 'Please enter date with a valid format: mm/dd/yyyy'
+  }
+}
+
+const validators = {
+  validateInput(regex, fieldValue) {
+    if(regex.test(fieldValue)){ return true; }
+  }
+}
+
+const view = {
+  createMessage(domPath, message) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = message;
+    paragraph.className = 'error';
+    domPath.insertAdjacentElement('afterend', paragraph);
+
+  },
+
+  checkIfErrorMessageExists(parentSibling) {
+    if(parentSibling.className === 'error') {
+      return true;
+      }
+  },
+
+  removeErrorMessage(parentSibling) {
+    if(this.checkIfErrorMessageExists(parentSibling)) {
+       parentSibling.remove();
+    }
+  }
+}
 
 // eslint-disable-next-line func-names
+/* 
 birthdayDateField.addEventListener('focus', function (e) {
   // hide any icon appearing in the text field
   this.parentNode.querySelector('.icon').classList.add('hidden');
@@ -51,3 +87,53 @@ eventsList.forEach((event) => birthdayDateField.addEventListener(event, function
     }
 }
 }));
+
+*/
+
+function validateInput(event) {
+  let target = event.target;
+  let targetId = event.target.id;
+  let parent = event.target.parentNode;
+  let parentSibling = event.target.parentNode.nextElementSibling;
+  switch (targetId) {
+    case 'birthday-date':
+      const {regex, message} = fieldsData.birthdayField;
+      validate = validators.validateInput(regex, birthdayDateField.value);
+      if(validate) {
+        view.removeErrorMessage(parentSibling);
+        // show checkmark icon
+        parent.querySelector('.icon-checkmark-outline').classList.remove('hidden');
+        target.classList.add('has-success');
+
+        // remove close/error icon
+        parent.querySelector('.icon-close-outline').classList.add('hidden');
+        // remove error outline
+        target.classList.remove('has-error');
+      }
+      else {
+        if(!view.checkIfErrorMessageExists(parentSibling)) {
+        view.createMessage(parent, message);
+        }
+        // hide error/checkmark icon 
+        parent.querySelector('.icon-checkmark-outline').classList.add('hidden');
+        // show error icon
+        parent.querySelector('.icon-close-outline').classList.remove('hidden');
+        target.classList.add('has-error');
+        target.classList.remove('has-success');
+      }
+      break;
+    default:
+      console.log(' aim nothing');
+      break;
+  }
+}
+
+const eventsList = ['input', 'blur'];
+
+formInputs.forEach(inputField => {
+  for (let event of eventsList) {
+    inputField.addEventListener(event, function(e) {
+      validateInput(e);
+    });
+  }
+});
